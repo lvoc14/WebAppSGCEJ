58c0311678c60404b3eb9d2a1288b021e7dd13fe
leonardo victor <leonardovictor5@hotmail.com>
2018-04-10 12:25:13 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGCEJ.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SGCEJ.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+                
+        }
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string CPF { get; set; }
+
+        public string RG { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Telefone   { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Email { get; set; }
+
+
+
+
+
+
+    }
+}
